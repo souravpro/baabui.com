caf1375eff0c904fe5506377357381a69efcdc9d
souravpro <45532872+souravpro@users.noreply.github.com>
2019-01-10 12:18:44 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+jQuery(document).ready(function($){
+    $(".school-list-slide").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true
+    });
+});
+
+
+jQuery(document).ready(function($){
+    $(".our-product-slide").owlCarousel({
+        items: 5,
+        margin: 30,
+        loop: true,
+        nav: true,
+        dots: true,
+        autoplay: true
+    });
+});
